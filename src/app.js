46392801d208e86d0d7c17dc660a55ca026034b8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-08 03:01:14 +0500

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 import express from "express";
+
+
+
+
+
 const app = express()
-export app
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN, // Allow only this origin
+    methods: 'GET,POST',           // Allow only GET and POST methods
+    allowedHeaders: 'Content-Type,Authorization' // Allow only specific headers
+};
+app.use(cors())
+
+
+
+export {app}

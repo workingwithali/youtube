03b298c2697686487547ae85ca2e25eeec8fec38
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-08 03:11:21 +0500

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 
 
@@ -14,6 +15,7 @@ app.use(cors(corsOptions))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 
 

a0dbdb3f6cbdae4a508768e922c414c10eaf5a71
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-28 13:37:08 +0500

    update

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,42 @@
 import mongoose , {Schema} from 'mongoose'
 
-const VideoSchema = new Schema({
-
-})
+const VideoSchema = new Schema(
+    {
+        videoFile:{
+            type: String,
+            required: true
+        },
+        thumnail:{
+            type: String,
+            required:true
+        },
+        title:{
+            type: String,
+            required:true
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        duration:{
+            type: Number,
+            required:true 
+        },
+        view:{
+            type: Number,
+            default: 0
+        },
+        isPublished:{
+            type : Boolean,
+            default: true
+        },
+        owner:{
+            type: mongoose.Schema.ObjectId,
+            ref : "User"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
  export const Video = mongoose.model('Video',VideoSchema)
